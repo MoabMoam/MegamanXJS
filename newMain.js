387db3d61c3f117714e1c5fbf261b6e6e0174824
Pedrovisck <52813792+pedroksty@users.noreply.github.com>
2019-10-14 23:13:40 -0300

    version 0.6
    Espinho com movimentação autônoma e com sensor de colisão

--- newMain.js
+++ newMain.js
@@ -8,7 +8,10 @@ var tile1 = new Image();
 var megamanX;
 var num_posicoes;
 var frames;
-var maxPulos = 2;
+var maxPulos = 3;
+var refX = document.getElementById("posX");
+var refY = document.getElementById("posY");
+var contEspinho = 1;
 
 // Recursos
 var background = new Image();
@@ -19,6 +22,8 @@ var logo = new Image();
 logo.src = "_play.png";
 var lose = new Image()
 lose.src = "perdeu.png"
+var thorn = new Image()
+thorn.src = "_espinho.png";
 
 // Estados
 var _estadoAtual;  
@@ -55,7 +60,7 @@ var gameWorld = { // Mundo
 
 var char = { // Mega Man
     img: player,
-    x: 0,
+    x: 310,
     y: 0,
     width: 85,
     height: 98,
@@ -80,36 +85,89 @@ var char = { // Mega Man
         this.qntPulos += 1
       }
     },
-  };
+};
 
-  //centralizando a o personagem
-  var cam = {
-    x: 0,
-    y: 0,
-    width: 800,
-    height: 600,
-    leftEdge: function() {
-      return this.x + this.width * 0.25;
-    },
-    rightEdge: function() {
-      return this.x + this.width * 0.75;
-    },
-    topEdge: function() {
-      return this.y + this.height * 0.25;
-    },
-    bottomEdge: function() {
-      return this.x + this.height * 0.75;
+var espinho = { // Espinho
+  img: thorn,
+  x: 50,
+  y: 0,
+  width: 15,
+  height: 25,
+  velocidade: 5
+};
+
+// Centralizando  o personagem
+var cam = {
+  x: 0,
+  y: 0,
+  width: 800,
+  height: 600,
+  leftEdge: function() {
+    return this.x + this.width * 0.25;
+  },
+  rightEdge: function() {
+    return this.x + this.width * 0.75;
+  },
+  topEdge: function() {
+    return this.y + this.height * 0.25;
+  },
+  bottomEdge: function() {
+    return this.x + this.height * 0.75;
+  }
+};
+
+// centralizando a câmera
+cam.x = (gameWorld.width - cam.width) / 2;
+cam.y = (gameWorld.height - cam.height) / 2;
+
+// Movendo o jogador
+var mvLeft = (mvRight = mvUp = mvDown = false);
+window.document.addEventListener(
+  "keydown",
+  function(e) {
+    var key = e.keyCode;
+    switch (key) {
+      case 37:
+        mvLeft = true;
+        break;
+      case 39:
+        mvRight = true;
+        break;
+      case 88:
+        mvUp = true;
+        break;
+      case 40:
+        mvDown = true;
+        break;
     }
-  };
+  },
+    false
+);
+window.document.addEventListener(
+  "keyup",
+  function(e) {
+    var key = e.keyCode;
+    switch (key) {
+      case 37:
+        mvLeft = false;
+        break;
+      case 39:
+        mvRight = false;
+        break;
+      case 88:
+        mvUp = false;
+        break;
+      case 40:
+        mvDown = false;
+        break;
+    }
+  },
+    false,
+);
 
-  // centralizando a câmera
-  cam.x = (gameWorld.width - cam.width) / 2;
-  cam.y = (gameWorld.height - cam.height) / 2;
-
-  // Movendo o jogador
-  var mvLeft = (mvRight = mvUp = mvDown = false);
-  window.document.addEventListener(
-    "keydown",
+var mvLeft = (mvRight = mvUp = mvDown = false);
+window.document.addEventListener(
+  "keydown",
     function(e) {
       var key = e.keyCode;
       switch (key) {
@@ -125,55 +183,10 @@ var char = { // Mega Man
         case 40:
           mvDown = true;
           break;
-      }
+        }
     },
     false
-  );
-  window.document.addEventListener(
-    "keyup",
-    function(e) {
-      var key = e.keyCode;
-      switch (key) {
-        case 37:
-          mvLeft = false;
-          break;
-        case 39:
-          mvRight = false;
-          break;
-        case 88:
-          mvUp = false;
-          break;
-        case 40:
-          mvDown = false;
-          break;
-      }
-    },
-    false,
-  );
-
-//movendo o jogador
-var mvLeft = (mvRight = mvUp = mvDown = false);
-window.document.addEventListener(
-      "keydown",
-      function(e) {
-        var key = e.keyCode;
-        switch (key) {
-          case 37:
-            mvLeft = true;
-            break;
-          case 39:
-            mvRight = true;
-            break;
-          case 88:
-            mvUp = true;
-            break;
-          case 40:
-            mvDown = true;
-            break;
-        }
-      },
-      false
-    );
+);
 window.document.addEventListener(
       "keyup",
       function(e) {
@@ -198,60 +211,60 @@ window.document.addEventListener(
  
 function movendoChar() {
     if (mvLeft && !mvRight) {
-        refX.textContent = char.x;
         char.x -= 4;
     }
     if (mvRight && !mvLeft) {
-        refX.textContent = char.x;
         char.x += 4;
     }
     if (mvUp && !mvDown) {
-        refY.textContent = char.y;
         char.pula()
     }
     if (mvDown && !mvUp) {
-        refY.textContent = char.y;
-      _estadoAtual = _estados.perdeu
+      
     }
 }
 
 // Limite da Câmera
 function limiteCam() {
-    if (cam.x < 0) {
-        cam.x = 0;
-    }
-    if (cam.x + cam.widght > gameWorld.width) {
-        cam.x = gameWorld.width - cam.width;
-    }
-    if (cam.y < 0) {
-        cam.y = 0;
-    }
-    if (cam.y + cam.height > gameWorld.height) {
-        cam.y = gameWorld.height - cam.height;
-    }
+  if (cam.x < 0) {
+    cam.x = 0;
   }
+  if (cam.x + cam.widght > gameWorld.width) {
+    cam.x = gameWorld.width - cam.width;
+  }
+  if (cam.y < 0) {
+    cam.y = 0;
+  }
+  if (cam.y + cam.height > gameWorld.height) {
+    cam.y = gameWorld.height - cam.height;
+  }
+}
 
 // Limite do Mega Man
 function limiteChar() {
-    if (char.x < 49) {
-        char.x = 49;
-    }
-    if (char.x > 670) {
-        char.x = 670;
-    }
-    if (char.y < 60) {
-        char.y = 60;
-    }
-    if (char.y + char.height > gameWorld.height) {
-        
-    }
+  if (char.x < 49) {
+    char.x = 49;
+  }
+  if (char.x > 670) {
+    char.x = 670;
+  }
+  if (char.y < 60) {
+    char.y = 60;
+  }
+  if (char.y > 375) {
+    char.y = 375
+    char.qntPulos = 0
+  }
 }
 
 // Clique
 function clique(event) {
+  if (_estadoAtual == _estados.jogar) {
     _estadoAtual = _estados.jogando
+  }
 }
 
+// Função inicializadora
 function main() {
     width = 800
     height = 600
@@ -267,20 +280,20 @@ function main() {
 
     _estadoAtual = _estados.jogar
 
-    run()
 
+    run()
 }
 
+// Agrupamento de funções principais
 function run () {
     update()
     draw()
     movendoChar()
     limiteCam()
     limiteChar()
-
+    posAtual()
+    colisao()
     window.requestAnimationFrame(run)
-
-    console.log(_estadoAtual)
 }
 
 function update() {
@@ -288,21 +301,45 @@ function update() {
     char.atualiza()
 }
 
+// Desenhar
 function draw() {
-  ctx.drawImage(gameWorld.img, 0, 0, 800, 600);
+  ctx.drawImage(gameWorld.img, 0, 0, 800, 600); // Desenhar mundo
 
   if (_estadoAtual == _estados.jogar) {
-    ctx.drawImage(play.img, 270, 150, 310, 150);
+    ctx.drawImage(play.img, 270, 150, 310, 150); // Desenhar botão play
   } else if (_estadoAtual == _estados.jogando) {
-    ctx.drawImage(char.img, char.x, char.y, 85, 98);
+    if (contEspinho % 2 == 0) {  // Desenhar objeto indo e vindo
+    ctx.drawImage(espinho.img, espinho.x -= espinho.velocidade, 450, espinho.width, espinho.height);
+    if (espinho.x == 50) {
+      contEspinho += 1
+    }
+    }
+    if (contEspinho % 2  != 0) {
+      ctx.drawImage(espinho.img, espinho.x += espinho.velocidade, 450, espinho.width, espinho.height);
+    }
+    if (espinho.x == 735) {
+      contEspinho += 1
+    }
+    ctx.drawImage(char.img, char.x, char.y, char.width, char.height); // Desenhar personagem principal
   } else if (_estadoAtual == _estados.perdeu) {
-    ctx.drawImage(perdeu.img, 270, 150, 310, 150);
+    ctx.drawImage(perdeu.img, 270, 150, 310, 150); // Desenhar botão de gamer over
   }
 }
 
-// Mostrar posição do jogador
-var refX = document.getElementById("posX");
-var refY = document.getElementById("posY");
+// Colisão
+function colisao() {
+  if (char.x > espinho.x - 77 && char.x < espinho.x + 15) {
+    if (char.y > 340) {
+      _estadoAtual = _estados.perdeu
+    }
+  }
+}
+
+// Posição atual do jogador
+function posAtual() {
+  refX.textContent = char.x;
+  refY.textContent = char.y;
+}
 
 // Inicializar o jogo
 main();
